6273c322df17c6d2afebf1574bfd503900a3d1e5
DaHerzog <DaHerzog@users.noreply.github.com>
2016-08-27 15:12:26 +0200

    Initial Commit

---
+++ Nostromo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Nostromo
+//
+//  Created by David Herzog on 27.08.16.
+//  Copyright © 2016 HS Osnabrueck. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
